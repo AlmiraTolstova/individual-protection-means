2a306d1a4c9543e533fd08e917380bf98957c0b3
AlmiraTolstova <almiratolstova@gmail.com>
2022-02-05 18:27:57 +0300

    react-redux установлен

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import '@mui/material';
 import '@mui/icons-material';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {ThemeProvider} from '@mui/styles'
+import store from "./store";
+import {Provider} from "react-redux";
 
 ReactDOM.render(
     <React.StrictMode>
-
+        <Provider store={store}>
             <App/>
-
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
 
 
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import {combineReducers, createStore} from "redux";
+
+// Наше хранилище
+
+const store = createStore(
+    combineReducers({
+
+    }),
+
+);
+
+export default store;
